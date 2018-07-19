1ed0af8e728721176ae0d4bb536cecfbb361c244
lehieu30794 <lehieu30794@gmail.com>
2018-07-19 11:18:47 -0600

    Finish phrases list with ArrayAdapter

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ArrayList<String> phrasesItem = new ArrayList<>();
+        phrasesItem.add("Where are you going?");
+        phrasesItem.add("What is your name?");
+        phrasesItem.add("My name is...");
+        phrasesItem.add("How are you feeling?");
+        phrasesItem.add("I’m feeling good.");
+        phrasesItem.add("Are you coming?");
+        phrasesItem.add("Yes, I’m coming.");
+        phrasesItem.add("I’m coming.");
+        phrasesItem.add("Let’s go.");
+        phrasesItem.add("Come here.");
+
+        ArrayAdapter<String> phrasesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, phrasesItem);
+
+        ListView phrasesListView = (ListView) findViewById(R.id.phrases_list);
+        phrasesListView.setAdapter(phrasesAdapter);
     }
 }
